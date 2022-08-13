92827c7374a58b0bfb39d05ebd90b94e1982f577
Alexander Waya Nguku <alex.waya@gmail.com>
2022-08-13 18:27:52 +0300

    config views

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,13 @@
+from django.urls import path
+from . import views
 
+# urlpatterns = [
+#     path("", views.ListListView.as_view(), name="index"),
+# ]
 
+# from todo_app import views
 
 urlpatterns = [
-    
+    path("", views.ListListView.as_view(), name="index"),
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
 ]
